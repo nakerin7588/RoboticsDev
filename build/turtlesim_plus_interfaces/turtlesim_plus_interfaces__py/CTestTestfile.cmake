592d3644eff9a210bb67040e145c1b2a233c1c97
SunnY <93282477+nakerin7588@users.noreply.github.com>
2024-08-25 18:49:23 +0700

    Add turtlesim_plus

---
+++ build/turtlesim_plus_interfaces/turtlesim_plus_interfaces__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nakarin/RoboticsDev/build/turtlesim_plus_interfaces/turtlesim_plus_interfaces__py
+# Build directory: /home/nakarin/RoboticsDev/build/turtlesim_plus_interfaces/turtlesim_plus_interfaces__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
